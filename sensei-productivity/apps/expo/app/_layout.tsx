272d6964150c49768c32df7cce79f9d0fdcba775
Kaiakanj1 <137309460+Kaiakanj1@users.noreply.github.com>
2026-02-23 08:14:58 -0500

    Revert "navigation_kaia"

--- sensei-productivity/apps/expo/app/_layout.tsx
+++ sensei-productivity/apps/expo/app/_layout.tsx
@@ -2,25 +2,9 @@ import { useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import { SplashScreen, Tabs } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
 import { Provider } from 'app/provider'
 import { NativeToast } from '@my/ui/src/NativeToast'
-import { StatusBar } from 'expo-status-bar';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { SenseiProductivity } from "@aurora-interactive/sensei-productivity"
-
-
-const senseiProductivity = new SenseiProductivity({
-  bearerAuth: process.env["SENSEIPRODUCTIVITY_BEARER_AUTH"] ?? "",
-})
-
-async function run() {
-  const result = await senseiProductivity.users.login({
-    username: "Kacey40",
-    password: "fbdGuMZsSr2v5wZ",
-  })
 
 export const unstable_settings = {
   // Ensure that reloading on `/user` keeps a back button present.
@@ -56,50 +40,7 @@ function RootLayoutNav() {
   return (
     <Provider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <StatusBar style="auto" />
-        <Tabs screenOptions={{ tabBarActiveTintColor: "teal", tabBarShowLabel: false}}>
-            <Tabs.Screen
-              name="index"
-              options={{
-                tabBarIcon: ({color, size}) => (
-                    <MaterialCommunityIcons
-                    name="home"
-                    size={size}
-                    color={color} />
-                )
-            }}/>
-            <Tabs.Screen name="feed"
-            options={{
-                tabBarIcon: ({color, size}) => (
-                    <MaterialCommunityIcons
-                    name="newspaper-variant-outline"
-                    size={size}
-                    color={color} />
-                )
-            }}/>
-            <Tabs.Screen name="challenge"
-            options={{
-                tabBarIcon: ({color, size}) => (
-                    <FontAwesome6
-                      name="trophy"
-                      size={size}
-                      color={color} />
-                )
-            }}/>
-            <Tabs.Screen name="profile"
-              options={{
-                tabBarIcon: ({color, size}) => (
-                    <FontAwesome
-                    name="user-circle"
-                    size={size}
-                    color={color} />
-                )
-              }}/>
-            <Tabs.Screen name="user/[id]"
-            options={{
-                href: null,
-            }}/>
-        </Tabs>
+        <Stack />
         <NativeToast />
       </ThemeProvider>
     </Provider>
--- sensei-productivity/apps/expo/app/index.tsx
+++ sensei-productivity/apps/expo/app/index.tsx
@@ -1,25 +1,12 @@
 import { HomeScreen } from 'app/features/home/screen'
 import { Stack } from 'expo-router'
-import { SenseiProductivity } from "@aurora-interactive/sensei-productivity"
-
-
-const senseiProductivity = new SenseiProductivity({
-  bearerAuth: process.env["SENSEIPRODUCTIVITY_BEARER_AUTH"] ?? "",
-})
-
-async function run() {
-  const result = await senseiProductivity.users.login({
-    username: "Kacey40",
-    password: "fbdGuMZsSr2v5wZ",
-  })
-}
 
 export default function Screen() {
   return (
     <>
       <Stack.Screen
         options={{
-          title: 'Tasks',
+          title: 'Home',
         }}
       />
       <HomeScreen />
