7c1b68d367ceced97aaf09af87dfc9d56d968b71
Antonios Papadakis <bdpaboi123@gmail.com>
2026-03-16 16:20:27 -0400

    Complete Social Feed

--- sensei-productivity/packages/app/features/feed/social-feed.tsx
+++ sensei-productivity/packages/app/features/feed/social-feed.tsx
@@ -9,6 +9,8 @@ import { GiftedChat } from 'react-native-gifted-chat'
 import { useState, useEffect, useRef } from 'react'
 import { SenseiProductivity } from '@aurora-interactive/sensei-productivity'
 import { map as promiseMap } from "bluebird";
+import Markdown from "react-native-markdown-display";
+import { MessageText } from "react-native-gifted-chat";
 
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -19,17 +21,32 @@ export function SocialFeedScreen() {
     const [error, setError] = useState("");
     const [accessToken, setAccessToken] = useState("");
     const [userId, setUserId] = useState(-1);
-    const [firstMessageId, setFirstMessageId] = useState(-1);
-    const [name, setName] = useState("");
     const [morePreviousMessagesAvailable, setMorePreviousMessagesAvailable] = useState(true);
     const [loadingMessages, setLoadingMessages] = useState(false);
+
     const loadingRef = useRef(true);
     const lastMessageIdRef = useRef(-1);
     const firstMessageIdRef = useRef(-1);
+    const userIdRef = useRef(-1);
+    const accessTokenRef = useRef("");
 
     const [messages, setMessages] = useState([])
 
-    const messageToGiftedChat = async (message, token) => {
+    const renderMessageText = (props) => {
+        const { currentMessage } = props;
+        if (currentMessage && currentMessage.text) {
+            return (
+                <Markdown style={{
+                    text: { color: props.position === 'left' ? 'black' : 'white' }
+                }}>
+                    {currentMessage.text}
+                </Markdown>
+            );
+        }
+        return <MessageText {...props} />; // default fallback
+    };
+
+    const messageToGiftedChat = async (message, token, currentUserId) => {
         const sdk = new SenseiProductivity({
             bearerAuth: `Bearer ${token}`
         });
@@ -44,10 +61,10 @@ export function SocialFeedScreen() {
 
             return {
                 _id: message.userId,
-                text: `${userData?.firstName} ${userData?.lastName} completed a ${postData?.categoryName} task!\n\n ${postData.caption ?? ""}`,
+                text: `${userData?.firstName} ${userData?.lastName} completed a **${postData?.categoryName}** task!\n\n ${postData.caption ?? ""}`,
                 createdAt: message.postDate,
                 user: {
-                    _id: 1,
+                    _id: userData.userId,
                     name: `${userData?.firstName} ${userData?.lastName}`
                 },
             };
@@ -76,6 +93,7 @@ export function SocialFeedScreen() {
             }
 
             setAccessToken(accessToken);
+            accessTokenRef.current = accessToken;
             loadingRef.current = false;
 
             const sdk = new SenseiProductivity({
@@ -86,7 +104,7 @@ export function SocialFeedScreen() {
             try {
                 profileInfo = await sdk.users.me();
                 setUserId(profileInfo.userId);
-                setName(`${profileInfo.firstName} ${profileInfo.lastName} `);
+                userIdRef.current = profileInfo.userId;
             } catch (e) {
                 console.log(e)
                 setError("Failed to get user profile. Restart the app and check your internet!");
@@ -105,16 +123,15 @@ export function SocialFeedScreen() {
                 if (newMessages?.length < 20) setMorePreviousMessagesAvailable(false);
                 if (newMessages?.length === 0) {
                     loadingRef.current = false;
+                    setLoadingMessages(false);
                     return;
                 }
 
-                setFirstMessageId(newMessages[0].postId);
+                firstMessageIdRef.current = newMessages[0].postId;
                 lastMessageIdRef.current = newMessages?.at(-1)?.postId;
 
-                const formattedMessages = await promiseMap(newMessages, msg => messageToGiftedChat(msg, accessToken));
-                console.log(formattedMessages)
+                const formattedMessages = await promiseMap(newMessages, msg => messageToGiftedChat(msg, accessToken, userId));
 
-                console.log("Setting messages :P")
                 setMessages(previousMessages =>
                     GiftedChat.append(previousMessages, formattedMessages),
                 );
@@ -135,69 +152,72 @@ export function SocialFeedScreen() {
         return (<Text style={{ color: "red" }}>{error ?? ""}</Text>)
     }
 
-    // useEffect(() => {
-    //     let timeoutId;
-    //     let isStopped = false; // Flag to prevent race conditions during cleanup
+    useEffect(() => {
+        let timeoutId;
+        let isStopped = false;
 
-    //     const fetchNewMessagesCallback = async () => {
-    //         console.log("Polling!")
-    //         if (lastMessageIdRef.current === -1) {
-    //             console.log("-1 message ID!")
-    //             if (!isStopped) {
-    //                 // Schedule the next call only after the current one finishes
-    //                 timeoutId = setTimeout(fetchNewMessagesCallback, 1000); // Poll every 3 seconds
-    //             }
-    //             return;
-    //         }
+        const fetchNewMessagesCallback = async () => {
+            if (firstMessageIdRef.current === -1) {
+                if (!isStopped) {
+                    timeoutId = setTimeout(fetchNewMessagesCallback, 1000);
+                }
+                return;
+            }
 
-    //         const sdk = new SenseiProductivity({
-    //             bearerAuth: `Bearer ${accessToken}`
-    //         });
+            const sdk = new SenseiProductivity({
+                bearerAuth: `Bearer ${accessTokenRef.current}`
+            });
 
-    //         while (loadingMessages) {
-    //             await sleep(1000);
-    //         }
+            while (loadingMessages) {
+                await sleep(1000);
+            }
 
-    //         setLoadingMessages(true);
+            setLoadingMessages(true);
 
-    //         const newMessages = await sdk.users.posts.feed({
-    //             lastPostId: lastMessageIdRef.current,
-    //             limit: 20
-    //         });
+            try {
+                const newMessages = await sdk.users.posts.feed({
+                    lastPostId: firstMessageIdRef.current,
+                    limit: 20
+                });
 
-    //         if (newMessages?.length < 20) setMorePreviousMessagesAvailable(false);
-    //         if (newMessages?.length === 0) {
-    //             console.log("No new messages!");
-    //             if (!isStopped) {
-    //                 // Schedule the next call only after the current one finishes
-    //                 timeoutId = setTimeout(fetchNewMessagesCallback, 1000); // Poll every 3 seconds
-    //             }
-    //             return;
-    //         }
+                if (newMessages?.length === 0) {
+                    console.log("No new messages!");
+                    if (!isStopped) {
+                        // schedule the next polling call
+                        timeoutId = setTimeout(fetchNewMessagesCallback, 1000);
+                    }
+                    setLoadingMessages(false);
+                    return;
+                }
 
-    //         const formattedMessages = await promiseMap(newMessages, messageToGiftedChat);
+                const formattedMessages = await promiseMap(newMessages, msg => messageToGiftedChat(msg, accessTokenRef.current, userIdRef.current));
 
-    //         lastMessageIdRef.current = newMessages.at(-1).postId;
-    //         setMessages(previousMessages =>
-    //             GiftedChat.append(previousMessages, formattedMessages),
-    //         );
+                firstMessageIdRef.current = newMessages.at(-1).postId;
+                setMessages(previousMessages =>
+                    GiftedChat.append(previousMessages, formattedMessages),
+                );
 
-    //         setLoadingMessages(false);
+                console.log("Captured new messages!");
+                setLoadingMessages(false);
+            } catch (e) {
+                console.log("Failed to poll for new messages!")
+                console.log(e)
+            }
 
-    //         if (!isStopped) {
-    //             // Schedule the next call only after the current one finishes
-    //             timeoutId = setTimeout(fetchNewMessagesCallback, 1000); // Poll every 3 seconds
-    //         }
-    //     };
+            if (!isStopped) {
+                // schedule next polling call
+                timeoutId = setTimeout(fetchNewMessagesCallback, 1000);
+            }
+        };
 
-    //     fetchNewMessagesCallback(); // Initial call
+        fetchNewMessagesCallback();
 
-    //     // Cleanup function to stop polling when the component unmounts or dependencies change
-    //     return () => {
-    //         isStopped = true;
-    //         clearTimeout(timeoutId);
-    //     };
-    // }, []);
+        // clean up when unmounting
+        return () => {
+            isStopped = true;
+            clearTimeout(timeoutId);
+        };
+    }, []);
 
     return (
         <>
@@ -205,6 +225,7 @@ export function SocialFeedScreen() {
                 messages={messages}
                 onSend={() => { }}
                 isScrollToBottomEnabled={morePreviousMessagesAvailable}
+                renderMessageText={renderMessageText}
                 loadEarlierMessagesProps={{
                     onPress: async () => {
                         while (loadingRef.current) {
@@ -231,7 +252,7 @@ export function SocialFeedScreen() {
 
                             lastMessageIdRef.current = feed?.at(-1)?.postId;
 
-                            const updatedMessages = await promiseMap(feed, async msg => await messageToGiftedChat(msg, accessToken));
+                            const updatedMessages = await promiseMap(feed, async msg => await messageToGiftedChat(msg, accessToken, userId));
                             setMessages(previousMessages =>
                                 GiftedChat.append(previousMessages, updatedMessages),
                             );
@@ -248,7 +269,7 @@ export function SocialFeedScreen() {
                     label: "Loading earlier social feed..."
                 }}
                 user={{
-                    _id: 1,
+                    _id: userId,
                 }}
             />
             <Button
