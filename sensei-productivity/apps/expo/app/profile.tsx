e6a627847f43cd15d6489119c68555330285d911
Antonios Papadakis <bdpaboi123@gmail.com>
2026-03-18 21:20:54 -0400

    Add Conditional Rendering For Login Screen
    Consequently also removes login screen from any flows in the authenticated section of the app

--- sensei-productivity/apps/expo/app/profile.tsx
+++ sensei-productivity/apps/expo/app/profile.tsx
@@ -1,4 +1,4 @@
-import { LoginScreen } from 'app/features/login/login-screen'
+import { ProfileScreen } from "app/features/profile/screen"
 import { Stack } from 'expo-router'
 
 export default function Screen() {
@@ -9,7 +9,7 @@ export default function Screen() {
           title: 'Profile',
         }}
       />
-      <LoginScreen />
+      <ProfileScreen />
     </>
   )
 }
